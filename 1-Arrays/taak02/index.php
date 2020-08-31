26ff12faa60f21295502ea7cbad8b8e03f01fd90
BruceAmende <bruceamende32@gmail.com>
2020-08-31 15:21:03 +0200

    taak 2/01 klaar

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,45 @@
+<?php
+
+ foreach ($medewerkers as $key => $value) {
+    echo $key;
+   }
+  $medewerkers = [
+    [
+      'voornaam' => 'Willem',
+      'achternaam' => 'van Oranje',
+      'functie'  => 'koning'
+    ],
+    [
+      'voornaam' => 'Donald',
+      'achternaam' => 'Trump',
+      'functie'  => 'president'
+    ],
+    [
+      'voornaam' => 'Homer',
+      'achternaam' => 'Simpson',
+      'functie'  => 'fabrieksarbeider'
+    ],
+    [
+      'voornaam' => 'Johan',
+      'achternaam' => 'Cruyff',
+      'functie'  => 'voetbalicoon'
+    ]
+    ,
+    [
+      'voornaam' => 'Badr',
+      'achternaam' => 'Hari',
+      'functie'  => 'Kickboxer'
+    ]
+  ];
+
+
+  echo $medewerkers[0]['voornaam']; 
+  echo $medewerkers[$x]['voornaam']; 
+  echo $medewerkers[$x]['voornaam'];
+
+
+
+
+
+
+?>
---
+++ 2-OOP/taak01/index.php
@@ -0,0 +1,15 @@
+<?php
+
+class __auto__(){
+    $merk;
+    $kleur;
+    $type;
+    $uitvoering;
+    $brandstof;
+  }
+  $nieuweAuto = new __auto__(); 
+
+  $nogEenAndereAuto = new __auto__();
+
+
+?>
