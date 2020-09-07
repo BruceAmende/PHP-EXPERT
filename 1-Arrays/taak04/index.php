22940836fd3a4775aa41e8e7b0245f10cdae7d88
BruceAmende <bruceamende32@gmail.com>
2020-09-07 11:17:13 +0200

    taak 1/04 klaar

--- 1-Arrays/taak04/index.php
+++ 1-Arrays/taak04/index.php
@@ -37,3 +37,44 @@ $games = [
         'prijs' => '40.00'
     ]
 ];
+
+echo $speelgoed[0]['naam'];
+echo "\n";
+echo $speelgoed[0]['soort'];
+echo "\n";
+echo $speelgoed[0]['prijs'];
+echo "\n  <br>";
+echo $speelgoed[1]['naam'];
+echo "\n";
+echo $speelgoed[1]['soort'];
+echo "\n";
+echo $speelgoed[1]['prijs'];
+echo "\n  <br>";
+echo $speelgoed[2]['naam'];
+echo "\n";
+echo $speelgoed[2]['soort'];
+echo "\n";
+echo $speelgoed[2]['prijs'];
+echo "\n  <br>";
+
+echo $games['sport']['naam'];
+echo "\n";
+echo $games['sport']['uitgever'];
+echo "\n";
+echo $games['sport']['prijs'];
+echo "\n  <br>";
+echo $games['action']['naam'];
+echo "\n";
+echo $games['action']['uitgever'];
+echo "\n";
+echo $games['action']['prijs'];
+echo "\n  <br>";
+echo $games['fps']['naam'];
+echo "\n";
+echo $games['fps']['uitgever'];
+echo "\n";
+echo $games['fps']['prijs'];
+
+
+
+
