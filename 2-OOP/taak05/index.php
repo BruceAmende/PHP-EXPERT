2de5e1c1cb02076aa112a1b1440c8e5fa44781a1
BruceAmende <bruceamende32@gmail.com>
2020-09-10 12:04:27 +0200

    taak 2/05 klaar

---
+++ 2-OOP/taak05/index.php
@@ -0,0 +1,27 @@
+<?php
+
+class Lamp{
+     public $kleur;
+      public $helderheid;
+     public $isAan;
+
+    function setStatus($status){ 
+        $this->isAan = $status;
+    }
+}
+$woonkamerLamp = new Lamp();
+$woonkamerLamp->kleur='geel';
+$woonkamerLamp->helderheid='zacht';
+$woonkamerLamp->isAan=true;
+$woonkamerLamp->setStatus(false);
+echo $woonkamerLamp->isAan;
+
+
+$keukenLamp = new Lamp();
+$keukenLamp->kleur='wit';
+$keukenLamp->helderheid='vel';
+$keukenLamp->isAan=false;
+
+
+
+?>
