4b206f5a39f72523df2c8c4eb5b1786bbc7f627c
BruceAmende <bruceamende32@gmail.com>
2020-09-16 15:26:10 +0200

    taak 4/02 klaar

---
+++ 4_PDO-INSERT/taak02/form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Add Record Form</title>
+</head>
+<body>
+<form action="create_location.php" method="post">
+    <p>
+        <label for="firstName">First Name:</label>
+        <input type="text" name="first_name" id="firstName">
+    </p>
+    <p>
+        <label for="lastName">Last Name:</label>
+        <input type="text" name="last_name" id="lastName">
+    </p>
+    <p>
+        <label for="lastName">Adres:</label>
+        <input type="text" name="adres" id="lastName">
+    </p>
+    <p>
+        <label for="lastName">Stad:</label>
+        <input type="text" name="stad" id="lastName">
+    </p>
+    
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
