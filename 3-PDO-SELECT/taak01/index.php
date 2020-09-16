548b8b357a3b637976c806013390e1fa7eff90cb
BruceAmende <bruceamende32@gmail.com>
2020-09-16 13:36:53 +0200

    taak 4/01 klaar

--- 3-PDO-SELECT/taak01/index.php
+++ 3-PDO-SELECT/taak01/index.php
@@ -4,13 +4,15 @@ $database_naam        = 'toolsforever';
 $database_gebruiker   = 'root';
 $database_wachtwoord  = '';
 
-$database_connectie = new PDO("mysql:host=$database_lokatie;dbname=$database_naam", $database_gebruiker, $database_wachtwoord);
+$db_conn = new PDO("mysql:host=$database_lokatie;dbname=$database_naam", $database_gebruiker, $database_wachtwoord);
 
 $sql = "SELECT * FROM users";
-
+$statement = $db_conn->prepare($sql); 
+$statement->execute();
+$database_gegevens = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 foreach($database_gegevens as $gebruiker){  
-    echo $gebruiker['naam'] . "<br>";
-  }
+  echo $gebruiker['firstname'] . "<br>";
+}
 
 ?>
