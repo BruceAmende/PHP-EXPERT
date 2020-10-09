22aa3f972ca069938e208d3e6abae96f01df6f3e
BruceAmende <bruceamende32@gmail.com>
2020-10-09 15:28:13 +0200

    eindopdracht volledig af en werkend

--- opdracht/db.conn/db.connUpdate.php
+++ opdracht/db.conn/db.connUpdate.php
@@ -9,10 +9,20 @@ $database_wachtwoord  = '';
 
 $db_conn = new PDO("mysql:host=$database_lokatie;dbname=$database_naam", $database_gebruiker, $database_wachtwoord);
 
-$sql = "UPDATE gebruikers SET voornaam='$_GET[voornaam]',achternaam='$_GET[achternaam]',email='$_GET[email]',wachtwoord='$_GET[wachtwoord]',team='$_GET[team]',contributie='$_GET[contributie]' where id=$id ";
+
+
+$sql = "UPDATE gebruikers SET voornaam= :voornaam,achternaam=:achternaam,email=:email,wachtwoord=:ww,team=:team,contributie=:contr where id=:id";
 $statement = $db_conn->prepare($sql); 
+$statement->bindParam(':voornaam', $_GET['voornaam']);
+$statement->bindParam(':achternaam', $_GET['achternaam']);
+$statement->bindParam(':email', $_GET['email']);
+$statement->bindParam(':ww', $_GET['wachtwoord']);
+$statement->bindParam(':team', $_GET['team']);
+$statement->bindParam(':contr', $_GET['contributie']);
+$statement->bindParam(':id', $_GET['id']);
+
 $statement->execute();
-$database_gegevens = $statement->fetchAll(PDO::FETCH_ASSOC);
+// $database_gegevens = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
